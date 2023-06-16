f20c1b246dea3b7b01410565b2b69df327867c06
Klara Fleischmann <fleischmann.kl@gmail.com>
2023-06-16 17:57:46 +0200

    chore: disable featureflags

--- remix.config.js
+++ remix.config.js
@@ -12,11 +12,11 @@ module.exports = {
   // serverModuleFormat: 'cjs',
   // tailwind: true,
   // serverBuildPath: "build/index.js",
-  future: {
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
+  // future: {
+  //   v2_errorBoundary: true,
+  //   v2_headers: true,
+  //   v2_meta: true,
+  //   v2_normalizeFormMethod: true,
+  //   v2_routeConvention: true,
+  // },
 };
