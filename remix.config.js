5ce747b5a2443a338e76b006ae7a5876d5e8346d
Klara Fleischmann <fleischmann.kl@gmail.com>
2023-06-16 17:52:11 +0200

    chore: 234987298374 try deployment

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,14 @@ module.exports = {
   ignoredRouteFiles: ['**/.*'],
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? './server.js'
+      ? './server.ts'
       : undefined,
   serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  publicPath: '/build/',
+  // serverModuleFormat: 'cjs',
+  // tailwind: true,
   // serverBuildPath: "build/index.js",
   future: {
     v2_errorBoundary: true,
