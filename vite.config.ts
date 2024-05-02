5293e10f82fb07f275a64e6ab09098b29162cbe9
Klara <fleischmann.kl@gmail.com>
2024-05-02 20:25:16 +0200

    fix: use new netlify setup properly �

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 import { vitePlugin as remix } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-installGlobals();
-
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths()],
+	plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
 });
